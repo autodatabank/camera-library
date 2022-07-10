3e5f82441e7272db6c9363e3c3a5694cbda56cd4
oooobang <oooobang@oooobangui-MacBookPro.local>
2022-07-10 23:55:17 +0900

    OutputFileOptions.Builder Extension 추가.

--- app/src/main/kotlin/kr/co/kadb/camera/presentation/widget/extension/ContextHelper.kt
+++ app/src/main/kotlin/kr/co/kadb/camera/presentation/widget/extension/ContextHelper.kt
@@ -3,17 +3,20 @@
 package kr.co.kadb.camera.presentation.widget.extension
 
 import android.app.Activity
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.ArrayRes
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
+import androidx.camera.core.ImageCapture
 import kr.co.kadb.camera.R
 import kr.co.kadb.camera.presentation.widget.util.IntentKey.BROADCAST_FINISH
 import kr.co.kadb.camera.presentation.widget.util.OBMediaScanning
+import timber.log.Timber
 import java.io.File
 
 /**
@@ -48,7 +51,10 @@ internal fun Context.getResourceString(@ArrayRes resourceId: Int, index: Int): S
 }
 
 // AlertDialog.
-internal fun Context.showAlert(message: CharSequence?, @StringRes titleId: Int = R.string.text_adb_camera_notify) {
+internal fun Context.showAlert(
+    message: CharSequence?,
+    @StringRes titleId: Int = R.string.text_adb_camera_notify
+) {
     showAlert {
         setTitle(titleId)
         setMessage(message)
@@ -56,7 +62,10 @@ internal fun Context.showAlert(message: CharSequence?, @StringRes titleId: Int =
 }
 
 // AlertDialog.
-internal fun Context.showAlert(@StringRes messageId: Int, @StringRes titleId: Int = R.string.text_adb_camera_notify) {
+internal fun Context.showAlert(
+    @StringRes messageId: Int,
+    @StringRes titleId: Int = R.string.text_adb_camera_notify
+) {
     showAlert {
         setTitle(titleId)
         setMessage(messageId)
@@ -144,4 +153,136 @@ internal inline fun Context.registerReceiver(
     }
     registerReceiver(receiver, intentFilter)
     return receiver
+}
+
+// 파일생성.
+internal fun Context?.createFile(
+    isPublicDirectory: Boolean = false,
+    filename: String = System.currentTimeMillis().toString(),
+    format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG
+): File? {
+    var path: String? = null
+    val extension = when (format) {
+        Bitmap.CompressFormat.PNG -> "png"
+        Bitmap.CompressFormat.JPEG -> "jpg"
+        else -> "webp"
+    }
+
+    if (isPublicDirectory) {
+        // 공유 저장소 사용 시 Android Q 대응.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/*")
+                put(MediaStore.Images.Media.DISPLAY_NAME, "$filename.$extension")
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    "${Environment.DIRECTORY_PICTURES}/adbcamerax"
+                )
+            }
+
+            this?.contentResolver?.let { contentResolver ->
+                // ContentResolver을 통해 insert.
+                val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                contentResolver.insert(collection, contentValues)?.let { uri ->
+                    // Debug.
+                    Timber.i(">>>>> VERSION_CODES >= Q URI : %s", uri)
+
+                    // 반환용.
+                    path = uri.toString()
+                }
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val directory = File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "signing"
+            )
+            if (!directory.mkdirs()) {
+                Timber.i(">>>>> Directory not created : %s", directory)
+            }
+
+            path = "${directory.absolutePath}/$filename.$extension"
+
+            // Debug.
+            Timber.i(">>>>> VERSION_CODES < Q PATH : %s", path)
+        }
+    } else {
+        // 내부 저장소 사용.
+        val directory = this?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        if (directory?.mkdirs() != true) {
+            Timber.i(">>>>> Directory not created : %s", directory)
+        }
+        path = "${directory?.absolutePath}/$filename.$extension"
+
+        // Debug.
+        Timber.i(">>>>> PATH : %s", path)
+    }
+
+    return path?.let {
+        File(it)
+    }
+}
+
+// OutputFileOptions.Builder 생성.
+internal fun Context?.outputFileOptionsBuilder(
+    isPublicDirectory: Boolean = false,
+    filename: String = System.currentTimeMillis().toString(),
+    format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG
+): ImageCapture.OutputFileOptions.Builder? {
+    val extension = when (format) {
+        Bitmap.CompressFormat.PNG -> "png"
+        Bitmap.CompressFormat.JPEG -> "jpg"
+        else -> "webp"
+    }
+
+    var builder: ImageCapture.OutputFileOptions.Builder? = null
+
+    if (isPublicDirectory) {
+        // 공유 저장소 사용 시 Android Q 대응.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues().apply {
+                //put(MediaStore.Images.Media.IS_PENDING, 1)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/$extension")
+                put(MediaStore.Images.Media.DISPLAY_NAME, "$filename.$extension")
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    "${Environment.DIRECTORY_PICTURES}/adbcamerax"
+                )
+            }
+
+            this?.contentResolver?.let { contentResolver ->
+                builder = ImageCapture.OutputFileOptions.Builder(
+                    contentResolver,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    contentValues
+                )
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val directory = File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "adbcamerax"
+            )
+            if (!directory.mkdirs()) {
+                Timber.i(">>>>> Directory not created : %s", directory)
+            }
+
+            builder = ImageCapture.OutputFileOptions.Builder(
+                File("${directory.absolutePath}/$filename.$extension")
+            )
+        }
+    } else {
+        // 내부 저장소 사용.
+        val directory = this?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        if (directory?.mkdirs() != true) {
+            Timber.i(">>>>> Directory not created : %s", directory)
+        }
+
+        builder = ImageCapture.OutputFileOptions.Builder(
+            File("${directory?.absolutePath}/$filename.$extension")
+        )
+    }
+
+    return builder
 }
