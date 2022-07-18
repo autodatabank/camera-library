e10996e2c7324fd618fa65843f3020d25256f5f9
oooobang <oooobang@oooobangui-MacBookPro.local>
2022-07-18 22:32:11 +0900

    테스트

--- cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/ui/shoot/ShootActivity.kt
+++ cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/ui/shoot/ShootActivity.kt
@@ -1,6 +1,10 @@
 package kr.co.kadb.cameralibrary.presentation.ui.shoot
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import kr.co.kadb.cameralibrary.R
 import kr.co.kadb.cameralibrary.presentation.base.BaseActivity
@@ -24,10 +28,46 @@ internal class ShootActivity : BaseActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.adb_cameralibrary_activity_shoot)
 
-//		viewModel.extras = intent.action
 		viewModel.intentAction(intent.action)
 		if (savedInstanceState == null) {
 			viewController.navigateToShooting()
+		}
+	}
+
+	override fun onWindowFocusChanged(hasFocus: Boolean) {
+		super.onWindowFocusChanged(hasFocus)
+
+		@Suppress("DEPRECATION")
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			window.setDecorFitsSystemWindows(true)
+			val controller = window.insetsController
+			if (controller != null) {
+				controller.hide(WindowInsets.Type.statusBars())// or WindowInsets.Type.navigationBars())
+				controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+			}
+		} else {
+			// All below using to hide navigation bar
+			val currentApiVersion = Build.VERSION.SDK_INT
+			val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					or View.SYSTEM_UI_FLAG_FULLSCREEN
+					or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+			// This work only for android 4.4+
+			if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+				window.decorView.systemUiVisibility = flags
+				// Code below is to handle presses of Volume up or Volume down.
+				// Without this, after pressing volume buttons, the navigation bar will
+				// show up and won't hide
+				val decorView = window.decorView
+				decorView.setOnSystemUiVisibilityChangeListener { visibility: Int ->
+					if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+						decorView.systemUiVisibility = flags
+					}
+				}
+			}
 		}
 	}
 }
