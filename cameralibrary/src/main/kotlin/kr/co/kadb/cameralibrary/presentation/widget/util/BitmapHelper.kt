8d74505ff347e9982727679291500110c6b0587e
oooobang <oooobang@oooobangui-MacBookPro.local>
2022-08-07 14:48:41 +0900

    add base64

--- cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/widget/util/BitmapHelper.kt
+++ cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/widget/util/BitmapHelper.kt
@@ -3,8 +3,10 @@ package kr.co.kadb.cameralibrary.presentation.widget.util
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
+import android.util.Base64
 import kr.co.kadb.cameralibrary.presentation.widget.extension.rotateAndCenterCrop
 import kr.co.kadb.cameralibrary.presentation.widget.extension.save
+import kr.co.kadb.cameralibrary.presentation.widget.extension.toBase64
 
 /**
  * Utility class for Bitmap.
@@ -20,6 +22,22 @@ class BitmapHelper {
             format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG
         ): String? {
             return bitmap?.save(context, isPublicDirectory, filename, format)
+        }
+
+        @JvmStatic
+        fun base64(
+            bitmap: Bitmap?
+        ): String? {
+            return bitmap.toBase64()
+        }
+
+        @JvmStatic
+        fun base64(
+            bitmap: Bitmap?,
+            flags: Int = Base64.NO_WRAP,
+            format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG
+        ): String? {
+            return bitmap.toBase64()
         }
     }
 }
