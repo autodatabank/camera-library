6f0abffca939dc3ef6287e48aa212ce74338b25c
oooobang <oooobang@oooobangKimui-Mac-Studio.local>
2022-08-09 16:40:40 +0900

    modify Example

---
+++ cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/domain/model/ShootItem.kt
@@ -0,0 +1,10 @@
+package com.intravan.signing.domain.model
+
+data class AgreeItem(
+    val companyCode: String,
+    val companyName: String,
+    val vehicleNumber: String,
+    val customerName: String,
+    val phoneNumber: String,
+    val serial: String
+)
