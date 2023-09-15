14d3e19b87e133330627d2facfa2caea7ac5f34a
oooobang <oooobangkim@gmail.com>
2023-09-15 15:27:55 +0900

    Fix: version

--- cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/widget/extension/IntentExtension.kt
+++ cameralibrary/src/main/kotlin/kr/co/kadb/cameralibrary/presentation/widget/extension/IntentExtension.kt
@@ -1,6 +1,8 @@
 package kr.co.kadb.cameralibrary.presentation.widget.extension
 
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import android.os.Parcelable
 import java.io.Serializable
 
@@ -26,4 +28,24 @@ fun <T : Parcelable?> Intent.getParcelable(name: String, clazz: Class<T>): T? {
         this.getParcelableExtra<T>(name)
     }*/
     return this.getParcelableExtra<T>(name)
+}
+
+inline fun <reified T : Serializable> Bundle.serializable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getSerializable(key) as? T
+}
+
+inline fun <reified T : Serializable> Intent.serializable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getSerializableExtra(key) as? T
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
+
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
